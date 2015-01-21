021a0654f57392c8a1ca6cf8f6b2f663080ed4dd
Skrusty <ben@xdev.net>
2015-01-21 11:21:14 +0000

    added missing files

---
+++ urlbot/BenBOT/Configuration/ConfigurationProviders/XmlConfigurationProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BenBOT.Configuration.ConfigurationProviders
+{
+    public class XmlConfigurationProvider : IConfigurationProvider
+    {
+        public void SaveConfiguration<T>(object configObject, string configName)
+        {
+            // Save Application Config File
+            var xs = new XmlSerializer(typeof(T));
+            using (var fs = File.Open(configName + ".xml", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                lock (configObject)
+                {
+                    xs.Serialize(fs, configObject);
+                }
+            }
+        }
+
+        public T LoadConfiguration<T>(string configName) where T: class
+        {
+            // Load Config File
+            var xs = new XmlSerializer(typeof(T));
+            try
+            {
+                using (var fs = File.OpenRead(configName + ".xml"))
+                {
+                    return (T)xs.Deserialize(fs);
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ urlbot/BenBOT/Configuration/IConfigurationProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BenBOT.Configuration
+{
+    public interface IConfigurationProvider
+    {
+        void SaveConfiguration<T>(object configObject, string configName);
+        T LoadConfiguration<T>(string configName) where T : class ;
+    }
+}
